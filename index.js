eee99ed4a4d0d633da156af8af64a30992dfd4b1
mbhornido <152159548+mbhornido@users.noreply.github.com>
2024-07-11 01:24:55 +0800

    second test

--- index.js
+++ index.js
@@ -1,82 +1,122 @@
-// getting Elements from Dom 
-const joinButton = document.querySelector("button");
-const audioContainer = document.getElementById("audioContainer");
-const textDiv = document.getElementById("textDiv");
-
-// declare Variables
-let participants = [];
-let meeting = null;
-let localParticipantAudio;
-let remoteParticipantId = "";
-
-joinButton.addEventListener("click", () => {
-  joinButton.style.display = "none";
-  textDiv.textContent = "Please wait, we are joining the meeting";
-
-  window.VideoSDK.config("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI4OTUyM2M3Ni1iZmYzLTRlNDgtYTQ2MS0yYmUyN2Y3ZWUyMTEiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcyMDYzMDc4NiwiZXhwIjoxNzIwNzE3MTg2fQ.5VOyHUEl7oYmG82fnf2Y_KIsWtxpq59Vx-MhsWNmnNk"); // required
-  meeting = window.VideoSDK.initMeeting({
-    meetingId: "3n48-l9wm-6n2t", // required
-    name: "Mister's Org", // required
-    micEnabled: true, // optional, default: true
-    webcamEnabled: false, // optional, default: true
-  });
-
-  meeting.join();
-});
-
-function createAudioElement(pId) {
-  let audioElement = document.createElement("audio");
-  audioElement.setAttribute("autoPlay", "false");
-  audioElement.setAttribute("playsInline", "true");
-  audioElement.setAttribute("controls", "false");
-  audioElement.setAttribute("id", `a-${pId}`);
-  return audioElement;
-}
-
-function setTrack(stream, audioElement, participant, isLocal) {
-  if (stream.kind == "audio" && !isLocal) {
-    const mediaStream = new MediaStream();
-    mediaStream.addTrack(stream.track);
-    audioElement.srcObject = mediaStream;
-    audioElement
-      .play()
-      .catch((error) => console.error("audioElem.play() failed", error));
+ // getting Elements from Dom 
+ const joinButton = document.querySelector("button");
+ const videoContainer = document.getElementById("videoContainer");
+ const textDiv = document.getElementById("textDiv");
+ 
+ // decalare Variables
+ let participants = [];
+ let meeting = null;
+ let localParticipant;
+ let localParticipantAudio;
+ let remoteParticipantId = "";
+ 
+ joinButton.addEventListener("click", () => {
+   joinButton.style.display = "none";
+   textDiv.textContent = "Please wait, we are joining the meeting";
+ 
+   window.VideoSDK.config("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI4OTUyM2M3Ni1iZmYzLTRlNDgtYTQ2MS0yYmUyN2Y3ZWUyMTEiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcyMDYzMDc4NiwiZXhwIjoxNzIwNzE3MTg2fQ.5VOyHUEl7oYmG82fnf2Y_KIsWtxpq59Vx-MhsWNmnNk") // required;
+   meeting = window.VideoSDK.initMeeting({
+     meetingId: "3n48-l9wm-6n2t", // required
+     name: "Mister's Org", // required
+     micEnabled: true, // optional, default: true
+     webcamEnabled: true, // optional, default: true
+   });
+ 
+   meeting.join();
+ });
+// =========================================
+  // creating video element
+  function createVideoElement(pId) {
+    let videoElement = document.createElement("video");
+    videoElement.classList.add("video-frame");
+    videoElement.setAttribute("id", `v-${pId}`);
+    videoElement.setAttribute("playsinline", true);
+    videoElement.setAttribute("width", "300");
+    return videoElement;
   }
-}
-
-joinButton.addEventListener("click", () => {
-  // creating local participant audio
-  localParticipantAudio = createAudioElement(meeting.localParticipant.id);
-  audioContainer.appendChild(localParticipantAudio);
-
-  // setting local participant stream
-  meeting.localParticipant.on("stream-enabled", (stream) => {
-    setTrack(
-      stream,
-      localParticipantAudio,
-      meeting.localParticipant,
-      (isLocal = true)
-    );
-  });
-
-  meeting.on("meeting-joined", () => {
-    textDiv.style.display = "none";
-  });
-
-  // other participants
-  meeting.on("participant-joined", (participant) => {
-    let audioElement = createAudioElement(participant.id);
-    remoteParticipantId = participant.id;
-
-    participant.on("stream-enabled", (stream) => {
-      setTrack(stream, audioElement, participant, (isLocal = false));
+  
+  // creating audio element
+  function createAudioElement(pId) {
+    let audioElement = document.createElement("audio");
+    audioElement.setAttribute("autoPlay", "false");
+    audioElement.setAttribute("playsInline", "true");
+    audioElement.setAttribute("controls", "false");
+    audioElement.setAttribute("id", `a-${pId}`);
+    return audioElement;
+  }
+  
+  // creating local participant
+  function createLocalParticipant() {
+    localParticipant = createVideoElement(meeting.localParticipant.id);
+    videoContainer.appendChild(localParticipant);
+  }
+  
+  // setting media track
+  function setTrack(stream, audioElement, participant, isLocal) {
+    if (stream.kind == "video") {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(stream.track);
+      let videoElm = document.getElementById(`v-${participant.id}`);
+      videoElm.srcObject = mediaStream;
+      videoElm
+        .play()
+        .catch((error) =>
+          console.error("videoElem.current.play() failed", error)
+        );
+    }
+    if (stream.kind == "audio" && !isLocal) {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(stream.track);
+      audioElement.srcObject = mediaStream;
+      audioElement
+        .play()
+        .catch((error) => console.error("audioElem.play() failed", error));
+    }
+  }
+  // ====================================
+  joinButton.addEventListener("click", () => {
+    // ...
+    // ...
+    // ...
+    
+    // creating local participant
+      createLocalParticipant();
+    
+    // setting local participant stream
+      meeting.localParticipant.on("stream-enabled", (stream) => {
+        setTrack(
+          stream,
+          localParticipantAudio,
+          meeting.localParticipant,
+          (isLocal = true)
+        );
+      });
+    
+      meeting.on("meeting-joined", () => {
+        textDiv.style.display = "none";
+      });
+    
+      // other participants
+      meeting.on("participant-joined", (participant) => {
+        let videoElement = createVideoElement(participant.id);
+        let audioElement = createAudioElement(participant.id);
+        remoteParticipantId = participant.id;
+    
+        participant.on("stream-enabled", (stream) => {
+          setTrack(stream, audioElement, participant, (isLocal = false));
+        });
+        videoContainer.appendChild(videoElement);
+        videoContainer.appendChild(audioElement);
+      });
+    
+      // participants left
+      meeting.on("participant-left", (participant) => {
+        let vElement = document.getElementById(`v-${participant.id}`);
+        vElement.parentNode.removeChild(vElement);
+    
+        let aElement = document.getElementById(`a-${participant.id}`);
+        aElement.parentNode.removeChild(aElement);
+        //remove it from participant list participantId;
+        document.getElementById(`p-${participant.id}`).remove();
+      });
     });
-    audioContainer.appendChild(audioElement);
-  });
-
-  // participants left
-  meeting.on("participant-left", (participant) => {
-    let aElement = document.getElementById(`a-${participant.id}`);
-    aElement.parentNode.removeChild(aElement);
-  });
-});
